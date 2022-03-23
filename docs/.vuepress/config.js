e6b2c8fd6463d5ead6ba8e9f75c7bdb9f42e6960
朱雁梁 <m63981996@126.com>
2022-03-23 23:18:21 +0800

    有bug

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,16 @@
 module.exports = {
+  title: 'Linger-UI',
+  base: '/lingerUI/',
   themeConfig: {
+    nav: [
+      {
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'GitHub'
+      }
+    ],
     sidebar: ['/', '/componentDocs/card']
   }
 }
